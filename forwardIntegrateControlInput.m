31322888f0ecc821d547a2f62cba08940c4d7884
DMCEWAN1 <dmcewan1@ford.com>
2017-12-04 18:09:16 -0500

    V2 of simulation files off canvas

--- forwardIntegrateControlInput.m
+++ forwardIntegrateControlInput.m
@@ -1,5 +1,5 @@
-function [Y] = forwardIntegrateControlInput(U,x0)
-%function [Y] = forwardIntegrateControlInput(U,x0)
+function [Y]=forwardIntegrateControlInput(U,x0)
+%function [Y]=forwardIntegrateControlInput(U,x0)
 %
 %Given a set of inputs and an initial condition, returns the vehicles
 %trajectory. If no initial condition is specified the default for the track
@@ -20,52 +20,53 @@ function [Y] = forwardIntegrateControlInput(U,x0)
 % Created: 13 Nov 2017
 % Modified: 16 Nov 2017
 
-    %if initial condition not given use default
-    if nargin<2
-        x0=[287,5,-176,0,2,0];
-    end
+%if initial condition not given use default
+if nargin<2
+    x0=[287,5,-176,0,2,0];
+end
 
-    %generate time vector
-    T=0:0.01:(size(U,1)-1)*0.01;
+%generate time vector
+T=0:0.01:(size(U,1)-1)*0.01;
 
-    %constants
-    W=13720;
-    Nw=2;
-    f=0.01;
-    Iz=2667;
-    a=1.35;
-    b=1.45;
-    By=0.27;
-    Cy=1.2;
-    Dy=2921;
-    Ey=-1.6;
-    Shy=0;
-    Svy=0;
-    m=1400;
+%constants
+W=13720;
+Nw=2;
+f=0.01;
+Iz=2667;
+a=1.35;
+b=1.45;
+By=0.27;
+Cy=1.2;
+Dy=2921;
+Ey=-1.6;
+Shy=0;
+Svy=0;
+m=1400;
 
-    %generate input functions
-    d_f=@(t) interp1(T,U(:,1),t,'previous','extrap');
-    F_x=@(t) interp1(T,U(:,2),t,'previous','extrap');
+%generate input functions
+d_f=@(t) interp1(T,U(:,1),t,'previous','extrap');
+F_x=@(t) interp1(T,U(:,2),t,'previous','extrap');
 
-    %slip angle functions in degrees
-    a_f=@(t,x) rad2deg(d_f(t)-atan2(x(4)+a*x(6),x(2)));
-    a_r=@(t,x) rad2deg(-atan2((x(4)-b*x(6)),x(2)));
+%slip angle functions in degrees
+a_f=@(t,x) rad2deg(d_f(t)-atan2(x(4)+a*x(6),x(2)));
+a_r=@(t,x) rad2deg(-atan2((x(4)-b*x(6)),x(2)));
 
-    %Nonlinear Tire Dynamics
-    phi_yf=@(t,x) (1-Ey)*(a_f(t,x)+Shy)+(Ey/By)*atan(By*(a_f(t,x)+Shy));
-    phi_yr=@(t,x) (1-Ey)*(a_r(t,x)+Shy)+(Ey/By)*atan(By*(a_r(t,x)+Shy));
+%Nonlinear Tire Dynamics
+phi_yf=@(t,x) (1-Ey)*(a_f(t,x)+Shy)+(Ey/By)*atan(By*(a_f(t,x)+Shy));
+phi_yr=@(t,x) (1-Ey)*(a_r(t,x)+Shy)+(Ey/By)*atan(By*(a_r(t,x)+Shy));
 
-    F_yf=@(t,x) Dy*sin(Cy*atan(By*phi_yf(t,x)))+Svy;
-    F_yr=@(t,x) Dy*sin(Cy*atan(By*phi_yr(t,x)))+Svy;
+F_yf=@(t,x) Dy*sin(Cy*atan(By*phi_yf(t,x)))+Svy;
+F_yr=@(t,x) Dy*sin(Cy*atan(By*phi_yr(t,x)))+Svy;
 
-    %vehicle dynamics
-    df=@(t,x) [x(2)*cos(x(5))-x(4)*sin(x(5));...
-              (-f*W+Nw*F_x(t)-F_yf(t,x)*sin(d_f(t)))/m+x(4)*x(6);...
-              x(2)*sin(x(5))+x(4)*cos(x(5));...
-              (F_yf(t,x)*cos(d_f(t))+F_yr(t,x))/m-x(2)*x(6);...
-              x(6);...
-              (F_yf(t,x)*a*cos(d_f(t))-F_yr(t,x)*b)/Iz];
+%vehicle dynamics
+df=@(t,x) [x(2)*cos(x(5))-x(4)*sin(x(5));...
+          (-f*W+Nw*F_x(t)-F_yf(t,x)*sin(d_f(t)))/m+x(4)*x(6);...
+          x(2)*sin(x(5))+x(4)*cos(x(5));...
+          (F_yf(t,x)*cos(d_f(t))+F_yr(t,x))/m-x(2)*x(6);...
+          x(6);...
+          (F_yf(t,x)*a*cos(d_f(t))-F_yr(t,x)*b)/Iz];
+      
+%Solve for trajectory      
+[~,Y]=ode45(df,T,x0);
 
-    %Solve for trajectory      
-    [~,Y] = ode45(df,T,x0);
 end
